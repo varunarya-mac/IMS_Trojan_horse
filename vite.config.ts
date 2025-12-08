25ba4b0976ab4bbacfb3c710a1d96a67fd05106f
Varun Arya <varun.arya@ims-evolve.com>
2025-12-08 18:51:52 +0000

    feat: logic added for git deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/IMS_Trojan_horse',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
