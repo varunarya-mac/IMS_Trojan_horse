3130a8ae093bee1da57516e84a641472b82ec3dc
Varun Arya <varun.arya@ims-evolve.com>
2025-12-19 07:44:22 +0000

    fix: vite config deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/IMS_Trojan_horse/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
